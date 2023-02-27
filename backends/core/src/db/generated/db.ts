4756a28b5e2a2c7add2700d369a71f627bde4c24
yashap <ypodeswa@gmail.com>
2023-02-26 16:08:23 -0800

    Tweak codegen

--- backends/core/src/db/generated/db.ts
+++ backends/core/src/db/generated/db.ts
@@ -6,31 +6,12 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
 
 export type Timestamp = ColumnType<Date, Date | string, Date | string>
 
-export interface _PrismaMigrations {
-  id: string
-  checksum: string
-  finished_at: Timestamp | null
-  migration_name: string
-  logs: string | null
-  rolled_back_at: Timestamp | null
-  started_at: Generated<Timestamp>
-  applied_steps_count: Generated<number>
-}
-
 export interface ParkingSpot {
   id: Generated<string>
   createdAt: Generated<Timestamp>
   updatedAt: Timestamp
   ownerUserId: string
   location: string | null
-}
-
-export interface SpatialRefSys {
-  srid: number
-  auth_name: string | null
-  auth_srid: number | null
-  srtext: string | null
-  proj4text: string | null
 }
 
 export interface User {
@@ -42,8 +23,6 @@ export interface User {
 }
 
 export interface DB {
-  _prisma_migrations: _PrismaMigrations
   ParkingSpot: ParkingSpot
-  spatial_ref_sys: SpatialRefSys
   User: User
 }
