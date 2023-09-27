962e39384a8f199ed6672c8a36ee03213ab6b7a6
yashap <ypodeswa@gmail.com>
2023-09-26 23:42:37 -0700

    Add error boundary

--- frontends/landlord/src/app/_layout.tsx
+++ frontends/landlord/src/app/_layout.tsx
@@ -3,29 +3,34 @@ import { StatusBar } from 'expo-status-bar'
 import FlashMessage from 'react-native-flash-message'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import 'react-native-url-polyfill/auto'
+import ErrorBoundary from '../components/ErrorBoundary'
 
 export default function Layout() {
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['top', 'left', 'right']} className='flex-1  bg-green-200'>
         <StatusBar style='auto' />
-        <Stack
-          screenOptions={{
-            headerBackTitle: '',
-            headerBackTitleVisible: false,
-            headerStyle: {
-              backgroundColor: 'rgb(187 247 208)',
-            },
-          }}
-        >
-          {/*
-            TODO: would be nicer to put this config in each component, but there's a bug where it generates
-            annoying warnings. Move these to the the individual screen components once that's fixed
-          */}
-          <Stack.Screen name='login' options={{ headerShown: false }} />
-          <Stack.Screen name='parkingSpots/list' options={{ title: 'Your Parking Spots' }} />
-          <Stack.Screen name='parkingSpots/new' options={{ title: 'Add Parking Spot' }} />
-        </Stack>
+        <ErrorBoundary>
+          {/* <AuthProvider> */}
+          <Stack
+            screenOptions={{
+              headerBackTitle: '',
+              headerBackTitleVisible: false,
+              headerStyle: {
+                backgroundColor: 'rgb(187 247 208)',
+              },
+            }}
+          >
+            {/*
+              TODO: would be nicer to put this config in each component, but there's a bug where it generates
+              annoying warnings. Move these to the the individual screen components once that's fixed
+            */}
+            <Stack.Screen name='login' options={{ headerShown: false }} />
+            <Stack.Screen name='parkingSpots/list' options={{ title: 'Your Parking Spots' }} />
+            <Stack.Screen name='parkingSpots/new' options={{ title: 'Add Parking Spot' }} />
+          </Stack>
+          {/* </AuthProvider> */}
+        </ErrorBoundary>
         <FlashMessage
           position='bottom'
           animated={true}
