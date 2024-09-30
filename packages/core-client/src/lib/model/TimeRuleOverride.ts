1e40e59bbd9a453998766c39d8c0faa2b49d5ec9
yashap <ypodeswa@gmail.com>
2024-09-29 17:08:44 -0700

    More flexible zod datetimes

--- packages/core-client/src/lib/model/TimeRuleOverride.ts
+++ packages/core-client/src/lib/model/TimeRuleOverride.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const TimeRuleOverrideSchema = z.object({
-  startsAt: z.string().datetime(),
-  endsAt: z.string().datetime(),
+  startsAt: z.string().datetime({ offset: true }),
+  endsAt: z.string().datetime({ offset: true }),
   isAvailable: z.boolean(),
 })
