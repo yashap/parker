2b5f3db78bc05fec713edc8cca546baeb50501b9
yashap <ypodeswa@gmail.com>
2024-11-10 15:58:29 +0100

    Add address field to frontend

--- frontends/landlord/src/app/parkingSpots/new.tsx
+++ frontends/landlord/src/app/parkingSpots/new.tsx
@@ -6,10 +6,11 @@ import { CoreClientBuilder } from '../../apiClient/CoreClientBuilder'
 import { useNavigationHeader } from '../../hooks/useNavigationHeader'
 import { showErrorToast } from '../../toasts/showErrorToast'
 
-const onSubmit = async ({ latitude, longitude }: { latitude: string; longitude: string }) => {
+const onSubmit = async ({ address, latitude, longitude }: { address: string; latitude: string; longitude: string }) => {
   const coreClient = CoreClientBuilder.build()
   try {
     await coreClient.parkingSpots.create({
+      address,
       location: { latitude: Number(latitude), longitude: Number(longitude) },
       // TODO!
       timeRules: [],
@@ -24,17 +25,19 @@ const onSubmit = async ({ latitude, longitude }: { latitude: string; longitude: 
 
 const NewParkingSpot: React.FC = () => {
   useNavigationHeader({ type: 'defaultHeader', title: 'New Parking Spot' })
+  const [address, setAddress] = useState('')
   const [latitude, setLatitude] = useState('')
   const [longitude, setLongitude] = useState('')
   return (
     <View className='items-stretch space-y-3 p-3'>
+      <TextInput label='Address' value={address} onChangeText={setAddress} />
       <TextInput label='Latitude' value={latitude} onChangeText={setLatitude} />
       <TextInput label='Longitude' value={longitude} onChangeText={setLongitude} />
       {/* TODO: maybe switch to Link component? https://docs.expo.dev/routing/navigating-pages/ */}
       <Button
         mode='contained'
         onPress={() => {
-          void onSubmit({ latitude, longitude })
+          void onSubmit({ address, latitude, longitude })
         }}
       >
         Submit
